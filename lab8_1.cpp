3907b8554c3f2fda6cd8256a3c6a6119700f4d72
mook <keettamook_k@cmu.ac.th>
2020-01-24 15:10:51 +0700

    finish 8_1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main(){	
+	float loan=0.00,rate=0.00,pay=0.00,nowR=0.00,newP=0.00;
+	int year=1;
+	cout << "Enter initial loan: ";
+	cin >> loan;
+    cout <<"Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -17,14 +25,26 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	for (;;){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;
+	nowR = (rate*loan)/100;
+	cout << setw(13) << left << nowR;
+	loan = nowR+loan;
+	cout << setw(13) << left << loan;
+	if(pay>loan){
+	newP=loan;
+	loan = loan - newP;
+	cout << setw(13) << left << newP;
+	}else{ cout << setw(13) << left << pay;
+	loan=loan-pay;}
+
+	cout << setw(13) << left << loan;
+	cout << "\n";
+	year++;
+	if(loan == 0)break;
+	}
 	
 	return 0;
 }
